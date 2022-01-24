f34a91ca8af14f758f533624b8262fb9e3be759d
Wojtek <w65143@student.wsiz.edu.pl>
2022-01-24 16:38:30 +0100

    test

---
+++ src/PositionTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+class PositionTest {
+
+
+    @org.junit.Test
+    public void setPosition() {
+        Position brickTest = new Position(1, 1);
+
+
+    }
+/*
+    @org.junit.Test
+    public void setBoundsSize() {
+        assertNull(1);
+    } */
+}

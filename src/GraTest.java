ffe5f0dfb442bb421c16a66555fddf0ceb5a4ed0
Wojtek <w65143@student.wsiz.edu.pl>
2022-01-24 17:38:00 +0100

    komentarz

---
+++ src/GraTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GraTest {
+
+    @Test
+    public void start() {
+
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void draw() {
+    }
+
+    @Test
+    public void addNotify() {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void run() {
+    }
+
+    @Test
+    public void keyTyped() {
+    }
+
+    @Test
+    public void keyPressed() {
+        assertTrue(right);
+    }
+
+    @Test
+    public void keyReleased() {
+    }
+}
--- src/PositionTest.java
+++ src/PositionTest.java
@@ -3,18 +3,15 @@ import org.junit.Test;
 import java.awt.*;
 
 import static org.junit.Assert.*;
-/*
 class PositionTest {
 
 
     @org.junit.Test
     public void setPosition() {
-        Position brickTest = new Position(1, 1);
-        return 1;
-
+        assertTrue(Rectangle);
 
     }
-    */
+}
 
 /*
     @org.junit.Test

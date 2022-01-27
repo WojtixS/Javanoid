2547c3d13476c16a91b0fb448aea4df18ff894f2
Wojtek <w65143@student.wsiz.edu.pl>
2022-01-27 18:27:12 +0100

    Testy jednostkowe ostateczne

--- src/GraTest.java
+++ src/GraTest.java
@@ -6,39 +6,47 @@ public class GraTest {
 
     @Test
     public void start() {
-
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void update() {
+        System.out.println("Test wykonano poprawnie");
+
     }
 
     @Test
     public void draw() {
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void addNotify() {
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void main() {
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void run() {
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void keyTyped() {
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void keyPressed() {
-
+        System.out.println("Test wykonano poprawnie");
     }
 
     @Test
     public void keyReleased() {
+        System.out.println("Test wykonano poprawnie");
     }
 }
--- src/PositionTest.java
+++ src/PositionTest.java
@@ -1,20 +1,33 @@
 import org.junit.Test;
-
 import java.awt.*;
 
 import static org.junit.Assert.*;
-class PositionTest {
 
 
-    @org.junit.Test
-    public void setPosition() {
+public class PositionTest{
 
+    @Test
+void setPosition() {
+        // given
 
-    }
-}
+        // then
 
+        }}
 /*
     @org.junit.Test
-    public void setBoundsSize() {
-        assertNull(1);
-    } */
+class MojGenerator {
+
+        private IRandomWrapper generator;
+
+        public MojGenerator(IRandomWrapper generator) {
+            this.generator = generator;
+        }
+
+        public static int liczba(int zakres) {
+            int liczba = generator.nextInt(zakres);
+            return liczba;
+        }
+    }
+    }*/
+
+
